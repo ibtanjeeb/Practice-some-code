a72f68f6dc64ea707a163a8202741d7d8dbac9ac
ibTanjeeb <threepointonefour1993@gmail.com>
2021-08-03 18:15:15 +0600

    Added new Files

---
+++ InterPolationSearch/InterPolationSearch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InterPolationSearch
+{
+    class Program
+    {
+        public static int Interpolationsearch(int[] arr, int lo,int hi, int x)
+        {
+            int pos;
+            if (lo <= hi && x>= arr[lo]&& x<= arr[hi])
+            {
+                pos = lo + (((hi - lo) /(arr[hi] - arr[lo])) *(x - arr[lo]));
+                if (arr[pos] == x)
+                    return pos;
+                if (arr[pos] < x)
+                    return Interpolationsearch(arr, pos + 1, hi, x);
+                if (arr[pos] > x)
+                    return Interpolationsearch(arr, lo, pos - 1, x);
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 6, 8, 10, 13 };
+            int n = arr.Length;
+            int x = 13;
+            int result = Interpolationsearch(arr, 0, n - 1, x);
+            if (result == -1)
+                Console.WriteLine("Not Found");
+            else
+                Console.WriteLine("Your index is " + result);
+        }
+    }
+}
---
+++ array 2d/array 2d/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace array_2d
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3];
+
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.WriteLine("Eneter the values{0} {1}:", i, j);
+                    arr[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            Console.WriteLine("Two Dimensional array Elements is:\n");
+
+            for(int i =0;i<2; i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.WriteLine(" " + arr[i,j]);
+                    if(j==2)
+                    {
+                        Console.WriteLine("elements");
+                    }
+                }
+            }
+        }
+    }
+}
