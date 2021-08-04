8a9b3e9db76d1791ba76590abe65323807dc2b81
ibTanjeeb <threepointonefour1993@gmail.com>
2021-08-04 23:16:17 +0600

    added new

---
+++ Middleware work/Middleware work/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Middleware_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args)=>
+        
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webbuilder =>
+            {
+                webbuilder.UseStartup<StartUp>();
+            }
+            );
+
+        
+    }
+}
---
+++ TwosumProblem/TwosumProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TwosumProblem
+{
+    class Program
+    {
+        public static int[] Twosums (int[] ar,int target)
+        {
+            int[] numbers = new int[2];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    else
+                    {
+                        if (ar[i] + ar[j] == target)
+                        {
+                            numbers[0] = i;
+                            numbers[1] = j;
+                        }
+
+                    }
+                }
+            }
+            return numbers;
+        }
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 3, 5, 6, 7 };
+            int target = 4;
+            var result = Twosums(ar, target);
+            Console.WriteLine(result[0]+" "+result[1] );
+        }
+    }
+}
