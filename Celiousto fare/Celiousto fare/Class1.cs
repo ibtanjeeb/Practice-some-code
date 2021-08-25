bcbf595576374c855665b8182bc9f3f69b711aac
ibTanjeeb <threepointonefour1993@gmail.com>
2021-08-25 23:01:34 +0600

    Added new

---
+++ Celiousto fare/Celiousto fare/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Celiousto_fare
+{
+    public class Class1
+    {
+        public  double Convert(Double amount)
+        {
+            double result = (amount * 9 / 5) + 32;
+
+            return result;
+        }
+
+
+    }
+}
---
+++ Celiousto fare/Celiousto fare/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Celiousto_fare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 clas = new Class1();
+
+            string userinput = Console.ReadLine();
+            double conin= double.Parse(userinput);
+
+            Console.WriteLine(clas.Convert(conin));
+
+
+
+        }
+
+
+       
+    }
+}
